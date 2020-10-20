7c6852076b136ba0c03f475627f165eb1dbb7612
Damou_ <mouadsehbaoui@gmail.com>
2020-10-20 16:59:08 +0200

    Feat: display a pokemon game depending on the query parameters send
    By sending a query param composed with version and the color it prints the pokemon game specified

--- src/Controller/HelloBilalController.php
+++ src/Controller/HelloBilalController.php
@@ -9,11 +9,15 @@ use Symfony\Component\Routing\Annotation\Route;
 class HelloBilalController extends AbstractController
 {
     /**
-     * @Route("/hello", name="hello_bilal")
+     * @Route("/pokemon", name="pokemon")
      */
     public function index()
     {
-        $Pokemon_games = array('Pokemon_red', 'Pokemon_blue', 'Pokemon_green');
+        $Pokemon_games = array('Pokemon_red' => 'Pokémon Red Version is the reprogrammed versions of Pocket Monsters Red',
+            'Pokemon_blue' => 'Pokémon Blue Version is the reprogrammed versions of Pocket Monsters Blue',
+            'Pokemon_green' => 'Pokémon Green Version is the reprogrammed versions of Pocket Monsters Green');
+        /*dd($Pokemon_games); dump and die function */
+        dump($Pokemon_games); //the dump fonction now is printed in the web debug tool bar .
         return $this->render('Pokemon.html.twig', [
             'Pokemon_games' => $Pokemon_games,
         ]);
