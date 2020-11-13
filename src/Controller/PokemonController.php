fd5355a2502c70bb1af1525af30966c4e203bc00
Damou_ <mouadsehbaoui@gmail.com>
2020-11-13 14:34:40 +0100

    Chore: code maintenance PokemonController
    Remove unused libraries

--- src/Controller/PokemonController.php
+++ src/Controller/PokemonController.php
@@ -4,20 +4,17 @@ namespace App\Controller;
 
 use App\Entity\Pokemon;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpClient\HttpClient;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Contracts\HttpClient\HttpClientInterface;
 
 class PokemonController extends AbstractController
 {
     /**
      * @Route("/pokemon/{id}", name="pokemon")
      * @param Pokemon $pokemon
-     * @param HttpClient $httpClient
      * @return Response
      */
-    public function index(Pokemon $pokemon, HttpClientInterface $httpClient): Response
+    public function index(Pokemon $pokemon): Response
     {
         return $this->render('pokemon/index.html.twig', [
             'controller_name' => 'PokemonController',
