1b89f6cb78ca30b8e47541de81dec5eb89a4296f
Damou_ <mouadsehbaoui@gmail.com>
2020-11-17 14:15:27 +0100

    Style : add new template file for index admin

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -2,8 +2,12 @@
 
 namespace App\Controller;
 
+use App\Entity\People;
+use App\Form\EditPeopleAdminType;
 use App\Repository\PeopleRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -30,9 +34,33 @@ class AdminController extends AbstractController
      */
     public function listUsers(PeopleRepository $peopleRepository)
     {
-        return $this->render("admin/index.html.twig", [
+        return $this->render("admin/ListUsers.html.twig", [
             'controller_name' => 'AdminController',
             'users' => $peopleRepository->findAll()
+        ]);
+    }
+
+    /**
+     * Edit a user's role and email address by any administrator.
+     * @Route ("/edit/user/{id}", name="edit_user")
+     * @param Request $request
+     * @param People $people
+     * @param EntityManagerInterface $entityManager
+     * @return Response
+     */
+    public function editUsers(Request $request, People $people, EntityManagerInterface $entityManager): Response
+    {
+        $form = $this->createForm(EditPeopleAdminType::class, $people);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $form->getData();
+            $entityManager->persist($form->getData());
+            $entityManager->flush();
+            $this->addFlash('success', 'data successfully updated');
+            return $this->redirectToRoute('admin_users', ['id' => $people->getId()]);
+        }
+        return $this->render('form_people/index.html.twig', [
+            'formPeople' => $form->createView(),
         ]);
     }
 }
